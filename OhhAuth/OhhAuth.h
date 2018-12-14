394359628ca1fa637a6fd5c74e28db110b24b1de
Sam Khawase <sam.khawse@gmail.com>
2018-12-14 12:28:40 +0100

    Carthage support
    moved the XcodeProj to root level
    
    minimum supported SDK is now iOS 10.0

---
+++ OhhAuth/OhhAuth.h
@@ -0,0 +1,19 @@
+//
+//  OhhAuth.h
+//  OhhAuth
+//
+//  Created by Saurabh Khawase on 14.12.18.
+//  Copyright © 2018 shūnya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OhhAuth.
+FOUNDATION_EXPORT double OhhAuthVersionNumber;
+
+//! Project version string for OhhAuth.
+FOUNDATION_EXPORT const unsigned char OhhAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OhhAuth/PublicHeader.h>
+
+
